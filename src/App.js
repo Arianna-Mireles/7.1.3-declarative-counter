70aa80db20af85438c9339f727e918a90adc97c1
Arianna Mireles <arianna.mireles02@gmail.com>
2023-11-14 21:33:56 -0600

    Third commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import React, { useState } from 'react'
 function App(){
     let [ counter, changeCounter ] = useState(1)
     window.changeCounter = changeCounter
+
+    const handleClick = () => {
+      changeCounter(counter + 1);
+    };
+
     return (
-        <div>
+        <div className='container'>
+          <button onClick={handleClick}>Click me!</button>
             <h1>Counter: {counter}</h1>
             <h4>Counter: {counter}</h4>
         </div>
